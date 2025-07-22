74166cebd8de7afdac1a1d3616088696254523cc
pontus3i <iii.pontus.com@gmail.com>
2025-07-22 14:47:07 +0700

    sửa logo với footer

--- components/Header/NarTop.tsx
+++ components/Header/NarTop.tsx
@@ -7,6 +7,7 @@ import { useDispatch } from "react-redux";
 import { setLanguage } from "@/Redux/languageSlice";
 import { useAppSelector } from "@/Redux/hook";
 import Link from "next/link";
+import logo from "@/Asset/image/logo.png";
 
 const NavTop = () => {
   const dispatch = useDispatch();
@@ -60,16 +61,20 @@ const NavTop = () => {
         {/* Logo */}
         <div className="flex items-center mb-4 md:mb-0">
           <div className="flex items-center">
-            {/* Globe Icon */}
-            <div className="w-8 h-8 bg-blue-600 rounded-full flex items-center justify-center mr-3">
-              <svg className="w-5 h-5 text-white" fill="currentColor" viewBox="0 0 24 24">
-                <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 17.93c-3.94-.49-7-3.85-7-7.93 0-.62.08-1.21.21-1.79L9 15v1c0 1.1.9 2 2 2v1.93zm6.9-2.54c-.26-.81-1-1.39-1.9-1.39h-1v-3c0-.55-.45-1-1-1H8v-2h2c.55 0 1-.45 1-1V7h2c1.1 0 2-.9 2-2v-.41c2.93 1.19 5 4.06 5 7.41 0 2.08-.8 3.97-2.1 5.39z"/>
-              </svg>
+            {/* Logo Image */}
+            <div className="w-32 h-16 mr-3">
+              <Image
+                src={logo}
+                width={128}
+                height={64}
+                alt="Logo"
+                className="w-full h-full object-contain"
+              />
             </div>
-            <div>
+            {/* <div>
               <h1 className="text-xl font-bold text-blue-600">PARIS LOGISTICS</h1>
               <p className="text-sm text-yellow-500 border-b border-yellow-500">PROFESSIONAL & LOGISTICS</p>
-            </div>
+            </div> */}
           </div>
         </div>
 
